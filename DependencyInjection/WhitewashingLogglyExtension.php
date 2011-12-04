975b49c42c87bb2553e86222cdedf729f04f2470
Benjamin Eberlei <kontakt@beberlei.de>
2011-12-04 12:03:41 +0100

    Bugfix in Extension

--- DependencyInjection/WhitewashingLogglyExtension.php
+++ DependencyInjection/WhitewashingLogglyExtension.php
@@ -22,9 +22,9 @@ class WhitewashingLogglyExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         
-        $container->setParameter('whitewashing_loggly.loggly.key', $config['whitewashing_loggly.key']);
-        $container->setParameter('whitewashing_loggly.loggly.host', $config['whitewashing_loggly.host']);
-        $container->setParameter('whitewashing_loggly.loggly.port', $config['whitewashing_loggly.port']);
+        $container->setParameter('whitewashing_loggly.loggly.key', $config['key']);
+        $container->setParameter('whitewashing_loggly.loggly.host', $config['host']);
+        $container->setParameter('whitewashing_loggly.loggly.port', $config['port']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
